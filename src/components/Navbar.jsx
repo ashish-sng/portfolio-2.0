d2896aeb907d4288cd39113279e0af4f21d3c939
Ashish Singh <ashishsingh.1899@gmail.com>
2023-03-05 11:17:21 +0530

    model done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,6 @@ const Navbar = () => {
           <div className={`${!toggle ? 'hidden':'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-x1`}>
             <ul className="list-none flex justify-end items-start flex-col gap-4">
               {navLinks.map((link) => {
-                console.log(link.title);
                 <li key={link.id}
                 className={`${active === link.title ? "text-white":"text-secondary"} font-poppins font-medium cursor-pointer text-[16px]]`}
                 onClick={()=> {

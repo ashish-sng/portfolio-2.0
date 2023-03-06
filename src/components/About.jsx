430d36d4ce658cf3ceda0809449c0e22e164db8d
Ashish Singh <ashishsingh.1899@gmail.com>
2023-03-06 14:05:00 +0530

    Experience section Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import {motion} from 'framer-motion'
 
 import {styles} from '../styles'
 import { services } from '../constants'
-import {fadeIn, textVariant} from '../utils/motion'
-import { SectionWrapper } from '../hoc'
+import {fadeIn, textVariant} from"../utils/motion"
+import { SectionWrapper } from "../hoc"
 
 const ServiceCard = ({index, title, icon}) => {
   return (
